c37b6cc02f2bb7e2ea4d7b1ec7c50189ca3b050d
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-12-16 19:44:06 +0300

    initial status reload

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-.PHONY: clean NUKE
+.PHONY: clean
 
-all: DSE
+all: 431project.cpp 431project.h 431projectUtils.cpp YOURCODEHERE.cpp
+	rm -rf DSE
+	g++ -O3 431project.cpp 431projectUtils.cpp YOURCODEHERE.cpp -lm -o DSE
 
 DSE: 431project.cpp 431project.h 431projectUtils.cpp YOURCODEHERE.cpp
 	g++ -O3 431project.cpp 431projectUtils.cpp YOURCODEHERE.cpp -lm -o DSE
 
 clean:
-	-rm DSE
+	rm -rf DSE
 
-NUKE:
-	-rm DSE; rm -rf rawProjectOutputData summaryfiles logs
--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -10,50 +10,149 @@
 #include <map>
 #include <math.h>
 #include <fcntl.h>
+#include <vector>
+#include <iterator>
 
 #include "431project.h"
 
-
-
+using namespace std;
 
 /*
- * Returns 1 if valid, else 0
+ * Enter your PSU IDs here to select the appropriate scanning order.
  */
-int validateConfiguration(std::string configuration){
-  int configurationDimsAsInts[18];
-  int returnValue=1; // assume true, set to zero otherwise
-  if(isan18dimconfiguration(configuration)){ // necessary, but insufficient
-    extractConfiguration(configuration, configurationDimsAsInts); // Configuration parameters now available in array
-    // FIXME - YOUR CODE HERE 
-  } else {
-    returnValue=0;
-  }
-  return returnValue;
+#define PSU_ID_SUM (900000800)
+
+/*
+ * Some global variables to track heuristic progress.
+ * 
+ * Feel free to create more global variables to track progress of your
+ * heuristic.
+ */
+unsigned int currentlyExploringDim = 0;
+bool currentDimDone = false;
+bool isDSEComplete = false;
+int visit2 = 0;
+std::string prevConfig;
+/*
+ * Given a half-baked configuration containing cache properties, generate
+ * latency parameters in configuration string. You will need information about
+ * how different cache paramters affect access latency.
+ * 
+ * Returns a string similar to "1 1 1"
+ */
+std::string generateCacheLatencyParams(string halfBackedConfig) {
+
+	string latencySettings;
+	//
+	//YOUR CODE BEGINS HERE
+	//
+	//
+	// Replace this dumb implementation.
+	latencySettings = "1 1 1";
+	//
+	//YOUR CODE ENDS HERE
+	//
+	return latencySettings;
 }
 
+/*
+ * Returns 1 if configuration is valid, else 0
+ */
+int validateConfiguration(std::string configuration) {
 
-
+	// FIXME - YOUR CODE HERE
+	return isNumDimConfiguration(configuration);
+}
 
 /*
- * Given the current best known configuration, the current configuration, and the globally visible map of all previously investigated configurations, suggest a previously unexplored design point. You will only be allowed to investigate 1000 design points in a particular run, so choose wisely.
+ * Given the current best known configuration, the current configuration,
+ * and the globally visible map of all previously investigated configurations,
+ * suggest a previously unexplored design point. You will only be allowed to
+ * investigate 1000 design points in a particular run, so choose wisely.
+ *
+ * In the current implementation, we start from the leftmost dimension and
+ * explore all possible options for this dimension and then go to the next
+ * dimension until the rightmost dimension.
  */
-std::string YourProposalFunction(std::string currentconfiguration, std::string bestEXECconfiguration, std::string bestEDPconfiguration, int optimizeforEXEC, int optimizeforEDP){
-  std::string nextconfiguration=GLOB_baseline;
-  /*
-   * REPLACE THE BELOW CODE WITH YOUR PROPOSAL FUNCTION
-   *
-   * The proposal function below is extremely unintelligent and
-   * will produce configurations that, while properly formatted, violate specified project constraints
-   */    
-  
-  // produces an essentially random proposal
-  std::stringstream ss;
-  for(int dim = 0; dim<17; ++dim){
-    ss << rand()%GLOB_dimensioncardinality[dim] << " ";
-  } 
-  ss << rand()%GLOB_dimensioncardinality[17];
-  nextconfiguration=ss.str();
-  ss.str("");    
-  return nextconfiguration;
+std::string generateNextConfigurationProposal(std::string currentconfiguration,
+		std::string bestEXECconfiguration, std::string bestEDPconfiguration,
+		int optimizeforEXEC, int optimizeforEDP) {
+
+	//
+	// Some interesting variables in 431project.h include:
+	//
+	// 1. GLOB_dimensioncardinality
+	// 2. GLOB_baseline
+	// 3. NUM_DIMS
+	// 4. NUM_DIMS_DEPENDENT
+	// 5. GLOB_seen_configurations
+	std::string nextconfiguration = currentconfiguration;
+	// Continue if proposed configuration is invalid or has been seen/checked before.
+	while (!validateConfiguration(nextconfiguration) ||
+		GLOB_seen_configurations[nextconfiguration]) {
+
+		// Check if DSE has been completed before and return current
+		// configuration.
+		if(isDSEComplete) {
+			return currentconfiguration;
+		}
+
+		std::stringstream ss;
+
+		string bestConfig;
+		if (optimizeforEXEC == 1)
+			bestConfig = bestEXECconfiguration;
+
+		if (optimizeforEDP == 1)
+			bestConfig = bestEDPconfiguration;
+
+		// Fill in the dimensions already-scanned with the already-selected best
+		// value.
+		for (int dim = 0; dim < currentlyExploringDim; ++dim) {
+			ss << extractConfigParam(bestConfig, dim) << " ";
+		}
+
+	
+		// Handling for currently exploring dimension. This is a very dumb
+		// implementation.
+		int nextValue = extractConfigParam(nextconfiguration, currentlyExploringDim) + 1;
+
+		if (nextValue >= GLOB_dimensioncardinality[currentlyExploringDim]) {
+			nextValue = GLOB_dimensioncardinality[currentlyExploringDim] - 1;
+			currentDimDone = true;
+		}
+		
+		ss << nextValue << " ";
+		
+		// Fill in remaining independent params with 0.
+		for (int dim = (currentlyExploringDim + 1);
+				dim < (NUM_DIMS - NUM_DIMS_DEPENDENT); ++dim) {
+			ss << "0 ";
+		}
+
+		//
+		// Last NUM_DIMS_DEPENDENT3 configuration parameters are not independent.
+		// They depend on one or more parameters already set. Determine the
+		// remaining parameters based on already decided independent ones.
+		//
+		string configSoFar = ss.str();
+
+		// Populate this object using corresponding parameters from config.
+		ss << generateCacheLatencyParams(configSoFar);
+
+		// Configuration is ready now.
+		nextconfiguration = ss.str();
+		
+		// Make sure we start exploring next dimension in next iteration.
+		if (currentDimDone) {
+			currentlyExploringDim++;
+			currentDimDone = false;
+		}
+		
+		// Signal that DSE is complete after this configuration.
+		if (currentlyExploringDim == (NUM_DIMS - NUM_DIMS_DEPENDENT))
+			isDSEComplete = true;
+	}
+	return nextconfiguration;
 }
 
